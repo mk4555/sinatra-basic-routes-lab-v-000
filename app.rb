21008a75529f3543d4f125271600a2b3a61c3b8d
Min Kim <mk4555@nyu.edu>
2018-01-12 05:26:58 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,18 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   get '/' do
-    
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Min Suk Kim"
+  end
+
+  get '/hometown' do
+    "My hometown is Johns Creek, Georgia"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is never really thought about my favorite song actually"
   end
 end
