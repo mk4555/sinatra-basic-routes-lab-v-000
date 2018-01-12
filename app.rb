a8eba40bb4624613234f0ed4672176664e6d0114
Min Kim <mk4555@nyu.edu>
2018-01-12 05:27:07 +0000

    Done.

--- app.rb
+++ app.rb
@@ -1,4 +1,19 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  get '/' do
+    "Hello, World!"
+  end
+
+  get '/name' do
+    "My name is Min Suk Kim"
+  end
+
+  get '/hometown' do
+    "My hometown is Johns Creek, Georgia"
+  end
+
+  get '/favorite-song' do
+    "My favorite song is never really thought about my favorite song actually"
+  end
 end
